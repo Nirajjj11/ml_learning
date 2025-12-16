cb5610c75c74c2c504280754c537b00c2d558464
Your Name <nirajkumar09160@gmail.com>
2025-12-16 16:46:07 +0530

    Multiple Regression done

---
+++ supervised_learning/multiple_regression_rent_prediction.py
@@ -0,0 +1,34 @@
+# Data set for people, Area in sq feet , age, income by using multiple regression predict the rent by input require area , age and income of the people.
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv("supervised_learning/data/rent.csv")
+
+print(df)
+X = df.drop(columns='rent')
+# print(X)
+y = df['rent']
+
+model = LinearRegression()
+model.fit(X,y)
+
+area = int(input("Required Area in sq feet : "))
+age = int(input("Enter age of the people : "))
+income = int(input("Enter income of the people : "))
+
+# predict_rent = model.predict([[area,age,income]])             # for directly use no need to input_df but for standard
+
+
+input_df = pd.DataFrame({
+    'area': [area],
+    'age': [age],
+    'income':[income]
+})
+
+predict_rent = model.predict(input_df)
+
+print("Predicted Rent is : ",predict_rent[0])
+
+print("Model Accuracy (R²):", model.score(X, y))
