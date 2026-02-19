497a59cd083e7231cc491a073fefdce605294d91
Your Name <nirajkumar09160@gmail.com>
2026-02-19 23:44:15 +0530

    test

--- supervised_learning/KNN_classifier_on_wave_data.py
+++ supervised_learning/KNN_classifier_on_wave_data.py
@@ -4,7 +4,7 @@ from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt 
 
 
-cancer = load_breast_cancer()
+cancer = load_breast_cancer() 
 
 X_train, X_test, y_train , y_test = train_test_split(cancer.data, cancer.target, stratify=cancer.target, random_state=66)
 
