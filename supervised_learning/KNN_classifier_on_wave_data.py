ac435074b7b5c6894e15aa54d169c35642ee9e12
Your Name <nirajkumar09160@gmail.com>
2026-04-04 23:48:29 +0530

    revision

--- supervised_learning/KNN_classifier_on_wave_data.py
+++ supervised_learning/KNN_classifier_on_wave_data.py
@@ -1,4 +1,4 @@
-from sklearn.datasets import 
+from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt 
